9b4a9c7617f84bfbe8a0256871d971786f536800
Matthew Zimmer <m16r01@gmail.com>
2019-01-19 20:38:20 -0500

    cleaned up graph.py a bit

--- app/lib/graph.py
+++ app/lib/graph.py
@@ -1,19 +1,24 @@
+import numpy as np
+
+
 class Graph:
-    def __init__(self, vertices):
+    def __init__(self, vertices=[]):
         self.vertices = vertices
 
-    def total_weight(weight):
-        return np.sum([ v.total_weight() for v in self.vertices])
+    def total_weight(self):
+        return np.sum([v.total_weight() for v in self.vertices])
+
 
 class Vertex:
-    def __init__(self,edge):
-        self.edge = edge
+    def __init__(self, edges=[]):
+        self.edges = edges
 
-    def total_weight(weight):
-        return np.sum([ v.total_weight() for v in self.edge])
+    def total_weight(self):
+        return np.sum([e.weight for e in self.edges])
+
 
 class Edge:
-    def __init(self,vertex1,vertex2 , weight):
+    def __init(self, vertex1, vertex2, weight):
         self.v1 = vertex1
         self.v2 = vertex2
         self.weight = weight
