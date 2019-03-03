9eb557c514f0476f12629b8f9c5777a82edbce8f
Matthew Zimmer <m16r01@gmail.com>
2019-03-03 12:40:34 -0500

    Adding user trajectory data up to uid 019

--- tests/lib/test_geolife_data_op.py
+++ tests/lib/test_geolife_data_op.py
@@ -6,4 +6,11 @@ def test_geolife_data():
 
     assert 'users' in data
     assert 'trajectories' in data
-    assert len(data['users']) == 20
+
+    notable_uids = set([
+        '000', '001', '002', '003', '004',
+        '005', '006', '007', '008', '009',
+        '010', '011', '012', '013', '014',
+        '015', '016', '017', '018', '019',
+    ])
+    assert set(data['users']).intersection(notable_uids) == notable_uids
