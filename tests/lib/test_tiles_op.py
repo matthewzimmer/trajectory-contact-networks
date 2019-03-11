dd8c6e4fc0fb365a8dce23626df5959e6728dfd8
Matthew Zimmer <m16r01@gmail.com>
2019-03-11 17:33:14 -0400

    incorporated hypothesis for arbitrary parameterized tests.

--- tests/lib/test_tiles_op.py
+++ tests/lib/test_tiles_op.py
@@ -1,21 +1,23 @@
 from app.lib.ops.tiles import GenerateTilesOp, GraphContactPointsOp
+from hypothesis import given, example
+import hypothesis.strategies as st
 
-def test_generate_user_tiles():
-    ds = 1000
-    dt = 1200
-    global_origin = (39.75872, 116.04142)
 
+@given(ds=st.integers(), dt=st.integers(), global_origin=st.tuples())
+@example(ds=1000, dt=1200, global_origin=(39.75872, 116.04142))
+@example(ds=500, dt=600, global_origin=(39.75872, 116.04142))
+@example(ds=100, dt=300, global_origin=(39.75872, 116.04142))
+def test_generate_user_tiles(ds, dt, global_origin):
     # Commented out due to Travis resource limitations
     tiles = GenerateTilesOp(ds, dt, global_origin).output()
     print(tiles)
 
 
-def test_generate_contact_points():
-    # select granularity
-    ds = 100  # 1000
-    dt = 300  # 1200
-    global_origin = (39.75872, 116.04142)
-
+@given(ds=st.integers(), dt=st.integers(), global_origin=st.tuples())
+@example(ds=1000, dt=1200, global_origin=(39.75872, 116.04142))
+@example(ds=500, dt=600, global_origin=(39.75872, 116.04142))
+@example(ds=100, dt=300, global_origin=(39.75872, 116.04142))
+def test_generate_contact_points(ds, dt, global_origin):
     # select choice of weight
     weight = 'count_weight'  # 'dist_weight'
     tiles = GenerateTilesOp(ds, dt, global_origin).output()
