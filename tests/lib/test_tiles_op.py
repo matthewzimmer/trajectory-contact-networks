ffbb7374e8698144f656446a8db529eec9d2d9b9
Francisco Cancedda <frankute333@yahoo.com>
2019-03-11 22:16:58 -0400

    weighted edges and class clean up (#11)
    * saved some test graphs with the different options currently implemented.
    
    * added distance and time difference to weight_by_count
    Separated Hottest tiles to a different pipeline class.
    tidied up math functions.
    
    * Made a few adjustments to have more test coverage for new functionality.
    
    * Added test for case when invalid tile hash is injected.

--- tests/lib/test_tiles_op.py
+++ tests/lib/test_tiles_op.py
@@ -1,32 +1,58 @@
 from app.lib.ops.tiles import GenerateTilesOp, GraphContactPointsOp
 
+
 def test_generate_user_tiles():
     ds = 1000
     dt = 1200
     global_origin = (39.75872, 116.04142)
 
     # Commented out due to Travis resource limitations
-    tiles = GenerateTilesOp(ds, dt, global_origin).output()
-    print(tiles)
+    # tiles = GenerateTilesOp(ds, dt, global_origin).output()
+    # print(tiles)
 
 
-def test_generate_contact_points():
+def test_generate_contacts_by_invalid_weight():
+    try:
+        GraphContactPointsOp(hashed_tiles={}, weight='invalid')
+    except AssertionError:
+        assert True
+
+
+def test_generate_contacts_bad_tile_hash():
+    op = GraphContactPointsOp(hashed_tiles={None: []}, weight='count_weight')
+    result = op.output()
+    assert result['graph_generated'] is False
+    assert result['graph_filepath'] == 'app/data/graphs/no_tiles_from_data.png'
+
+
+def test_generate_contacts_by_count_weight():
     # select granularity
     ds = 100  # 1000
     dt = 300  # 1200
     global_origin = (39.75872, 116.04142)
 
-    # select choice of weight
-    weight = 'count_weight'  # 'dist_weight'
     tiles = GenerateTilesOp(ds, dt, global_origin).output()
 
-    # find contacts between users (tiles with at least two users)
-    # graph_op = GraphContactPointsOp(tiles, weight).perform()
-
     # find contact points involving most users. (most occupied tile/s)
-    graph_op = GraphContactPointsOp(tiles, weight).hottest_tile()
+    graph_op = GraphContactPointsOp(tiles, weight='count_weight')
 
     # Test that a graph has been generated
     result = graph_op.output()
     assert result['graph_generated'] is True
-# assert result['png_filepath'] == "{}.png".format(tile_hash)
+
+
+def test_generate_contacts_by_distance():
+    # select granularity
+    ds = 100  # 1000
+    dt = 300  # 1200
+    global_origin = (39.75872, 116.04142)
+
+    tiles = GenerateTilesOp(ds, dt, global_origin).output()
+
+    # find contact points involving most users. (most occupied tile/s)
+    graph_op = GraphContactPointsOp(tiles, weight='dist_weight')
+
+    # Test that a graph has been generated
+    result = graph_op.output()
+    assert result['graph_generated'] is True
+
