83136cc7dc9f81c0dea43db28cfb76d444c209c3
Matthew Zimmer <m16r01@gmail.com>
2019-03-28 08:33:44 -0400

    Refactor complete.

--- tests/lib/test_tiles_op.py
+++ tests/lib/test_tiles_op.py
@@ -1,16 +1,22 @@
+from builtins import AssertionError
+
 from app.lib.ops.tiles import GenerateTilesOp, GraphContactPointsOp
 from hypothesis import given, example
 import hypothesis.strategies as st
+import numpy as np
+import pandas as pd
+
+ALL_USER_IDS = [f'{i:03d}' for i in range(0, 100)]
+GLOBAL_ORIGIN = (39.75872, 116.04142)
 
 
-@given(ds=st.integers(), dt=st.integers(), global_origin=st.tuples())
-@example(ds=1000, dt=1200, global_origin=(39.75872, 116.04142))
-@example(ds=500, dt=600, global_origin=(39.75872, 116.04142))
-@example(ds=100, dt=300, global_origin=(39.75872, 116.04142))
-def test_generate_user_tiles(ds, dt, global_origin):
+@given(users=st.lists(st.integers()), ds=st.integers(), dt=st.integers(), global_origin=st.tuples())
+@example(users=['000', '001'], ds=1000, dt=1200, global_origin=GLOBAL_ORIGIN)
+def test_generate_user_tiles(users, ds, dt, global_origin):
     # Commented out due to Travis resource limitations
-    # tiles = GenerateTilesOp(ds, dt, global_origin).output()
+    # tiles = GenerateTilesOp(users, ds, dt, global_origin).output()
     # print(tiles)
+    # assert len(tiles) == 5737
     pass
 
 
@@ -28,31 +34,38 @@ def test_generate_contacts_bad_tile_hash():
     assert result['graph_filepath'] == 'app/data/graphs/no_tiles_from_data.png'
 
 
-@given(ds=st.integers(), dt=st.integers(), global_origin=st.tuples())
-@example(ds=1000, dt=1200, global_origin=(39.75872, 116.04142))
-@example(ds=500, dt=600, global_origin=(39.75872, 116.04142))
-@example(ds=100, dt=300, global_origin=(39.75872, 116.04142))
-def test_generate_contacts_by_count_weight(ds, dt, global_origin):
-    tiles = GenerateTilesOp(ds, dt, global_origin).output()
+@given(users=st.lists(st.integers()), ds=st.integers(), dt=st.integers(), global_origin=st.tuples())
+@example(users=['000', '001'], ds=1000, dt=1200, global_origin=GLOBAL_ORIGIN)
+def test_generate_contacts_by_count_weight(users, ds, dt, global_origin):
+    if len(global_origin) == 0:
+        pass
+
+    tiles_op = GenerateTilesOp(users, ds, dt, global_origin)
 
     # find contact points involving most users. (most occupied tile/s)
-    graph_op = GraphContactPointsOp(tiles, weight='count_weight')
+    graph_op = GraphContactPointsOp(tiles_op.output(), weight='count_weight')
 
     # Test that a graph has been generated
     result = graph_op.output()
+
+    pd.DataFrame(result['contact_points']).to_csv("contact_points_by_count_weight_{}ds_{}dt.csv".format(ds, dt), header=None, index=None)
+
     assert result['graph_generated'] is True
 
 
-@given(ds=st.integers(), dt=st.integers(), global_origin=st.tuples())
-@example(ds=1000, dt=1200, global_origin=(39.75872, 116.04142))
-@example(ds=500, dt=600, global_origin=(39.75872, 116.04142))
-@example(ds=100, dt=300, global_origin=(39.75872, 116.04142))
-def test_generate_contacts_by_distance(ds, dt, global_origin):
-    tiles = GenerateTilesOp(ds, dt, global_origin).output()
+@given(users=st.lists(st.integers()), ds=st.integers(), dt=st.integers(), global_origin=st.tuples())
+@example(users=['000', '001'], ds=1000, dt=1200, global_origin=GLOBAL_ORIGIN)
+def test_generate_contacts_by_distance(users, ds, dt, global_origin):
+    if len(global_origin) == 0:
+        pass
+
+    tiles_op = GenerateTilesOp(users, ds, dt, global_origin)
 
     # find contact points involving most users. (most occupied tile/s)
-    graph_op = GraphContactPointsOp(tiles, weight='dist_weight')
+    graph_op = GraphContactPointsOp(tiles_op.output(), weight='dist_weight')
 
     # Test that a graph has been generated
     result = graph_op.output()
+    pd.DataFrame(result['contact_points']).to_csv("contact_points_by_distance_{}ds_{}dt.csv".format(ds, dt), header=None, index=None)
+
     assert result['graph_generated'] is True
